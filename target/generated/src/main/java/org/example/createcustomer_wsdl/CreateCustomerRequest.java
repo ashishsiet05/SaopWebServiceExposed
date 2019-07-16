de379b9fe878e80bf80e0aed0c45b445a2ef6dc6
ashish maurya <ashishmaurya.siet@gmail.com>
2019-07-16 21:16:15 +0530

    first commit

---
+++ target/generated/src/main/java/org/example/createcustomer_wsdl/CreateCustomerRequest.java
@@ -0,0 +1,372 @@
+
+package org.example.createcustomer_wsdl;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="CustID" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="FirstName" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="LastName" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="MiddleName" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="Email" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="PhoneNo" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="MobileNo" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="Add1" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="Add2" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="Add3" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="City" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="Pincode" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "custID",
+    "firstName",
+    "lastName",
+    "middleName",
+    "email",
+    "phoneNo",
+    "mobileNo",
+    "add1",
+    "add2",
+    "add3",
+    "city",
+    "pincode"
+})
+@XmlRootElement(name = "CreateCustomerRequest")
+public class CreateCustomerRequest {
+
+    @XmlElement(name = "CustID", required = true)
+    protected String custID;
+    @XmlElement(name = "FirstName", required = true)
+    protected String firstName;
+    @XmlElement(name = "LastName", required = true)
+    protected String lastName;
+    @XmlElement(name = "MiddleName", required = true)
+    protected String middleName;
+    @XmlElement(name = "Email", required = true)
+    protected String email;
+    @XmlElement(name = "PhoneNo", required = true)
+    protected String phoneNo;
+    @XmlElement(name = "MobileNo", required = true)
+    protected String mobileNo;
+    @XmlElement(name = "Add1", required = true)
+    protected String add1;
+    @XmlElement(name = "Add2", required = true)
+    protected String add2;
+    @XmlElement(name = "Add3", required = true)
+    protected String add3;
+    @XmlElement(name = "City", required = true)
+    protected String city;
+    @XmlElement(name = "Pincode", required = true)
+    protected String pincode;
+
+    /**
+     * Gets the value of the custID property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCustID() {
+        return custID;
+    }
+
+    /**
+     * Sets the value of the custID property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCustID(String value) {
+        this.custID = value;
+    }
+
+    /**
+     * Gets the value of the firstName property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Sets the value of the firstName property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setFirstName(String value) {
+        this.firstName = value;
+    }
+
+    /**
+     * Gets the value of the lastName property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Sets the value of the lastName property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setLastName(String value) {
+        this.lastName = value;
+    }
+
+    /**
+     * Gets the value of the middleName property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    /**
+     * Sets the value of the middleName property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setMiddleName(String value) {
+        this.middleName = value;
+    }
+
+    /**
+     * Gets the value of the email property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Sets the value of the email property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setEmail(String value) {
+        this.email = value;
+    }
+
+    /**
+     * Gets the value of the phoneNo property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    /**
+     * Sets the value of the phoneNo property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setPhoneNo(String value) {
+        this.phoneNo = value;
+    }
+
+    /**
+     * Gets the value of the mobileNo property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getMobileNo() {
+        return mobileNo;
+    }
+
+    /**
+     * Sets the value of the mobileNo property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setMobileNo(String value) {
+        this.mobileNo = value;
+    }
+
+    /**
+     * Gets the value of the add1 property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getAdd1() {
+        return add1;
+    }
+
+    /**
+     * Sets the value of the add1 property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setAdd1(String value) {
+        this.add1 = value;
+    }
+
+    /**
+     * Gets the value of the add2 property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getAdd2() {
+        return add2;
+    }
+
+    /**
+     * Sets the value of the add2 property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setAdd2(String value) {
+        this.add2 = value;
+    }
+
+    /**
+     * Gets the value of the add3 property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getAdd3() {
+        return add3;
+    }
+
+    /**
+     * Sets the value of the add3 property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setAdd3(String value) {
+        this.add3 = value;
+    }
+
+    /**
+     * Gets the value of the city property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * Sets the value of the city property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCity(String value) {
+        this.city = value;
+    }
+
+    /**
+     * Gets the value of the pincode property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getPincode() {
+        return pincode;
+    }
+
+    /**
+     * Sets the value of the pincode property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setPincode(String value) {
+        this.pincode = value;
+    }
+
+}
